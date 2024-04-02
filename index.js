3b4d9e1d4365074accb9006b8272f43cadbd862d
AnnaBaranek <161436619+AnnaBaranek@users.noreply.github.com>
2024-04-02 17:13:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const volno = dayjs('2024-04-03');
+let volno = dayjs('2024-04-03');
+let volno = dayjs('2024-04-04');
 const dnes = dayjs();
 
 document.body.innerHTML += `<h1 style="color: #880e4f;">Mám dnes volno?`;
