ba75433ca22b331a991765c27460ff414f754a15
AnnaBaranek <161436619+AnnaBaranek@users.noreply.github.com>
2024-04-02 17:57:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dnes = dayjs();
 document.body.innerHTML += `<h1 style="color: #880e4f;">Mám dnes volno?`;
 
 if (dnes.isAfter(volno)) {
-    document.body.innerHTML += 'ANO!Běž se učit';
+    document.body.innerHTML += 'ANO!Běž se učit!';
   } else {
     document.body.innerHTML += 'Ještě ne :(';
   }
